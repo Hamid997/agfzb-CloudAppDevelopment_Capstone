0d7f34bb0d031f5914210f5f7fb7784fc29eb79c
Abdelhamid <hamid.birouk0597@gmail.com>
2023-05-25 10:02:43 -0400

    Final

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,9 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ['localhost', 'localhost:8000', 'https://hamidbirouk0-8000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
+CSRF_TRUSTED_ORIGINS = ['https://hamidbirouk0-8000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 # Application definition
 
